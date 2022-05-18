a858224341c1c58e9e2ccc1de706ae93be12ab6b
nicolas2589 <46973275+nicolas2589@users.noreply.github.com>
2022-05-18 08:18:37 -0700

    Update settings.py
    Adding mysql database connection to aws

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'proyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Prueba1',
+        'USER': 'admin',
+        'PASSWORD': '1234567q.,',
+        'HOST': 'database-1.csas9v7nsqsf.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
