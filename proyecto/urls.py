d9ad122603b911fcb5911eedff50520ada834542
Ariel <ariel.landa17@tectijuana.edu.mx>
2022-05-29 00:28:41 -0700

    Requeriments file

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -40,13 +40,15 @@ urlpatterns = [
     path('versoporte/', views.VerSoporte.as_view(), name='verS'),
     path('modificarsoporte/<int:pk>/', views.ModificarSoporte.as_view(), name='modificarS'),
     path('eliminarsoporte/<int:pk>/', views.EliminarSoporte.as_view(), name='eliminarS'),
-    #Apis
+    #Apis de historial
     path('historial_equipo/<int:pk>', views.historial_equipo, name="he"),
     path('historial_cliente/<int:pk>', views.historial_cliente, name="hc"),
     path('historial_tecnico/<int:pk>', views.historial_tecnico, name="ht"),
+    #Apis de crear
     path('crear_cliente/', views.crear_cliente, name="cc"),
     path('crear_equipo/', views.crear_equipo, name="ce"),
     path('crear_soporte/', views.crear_soporte, name="cs"),
+    #Apis Varios
     path('cerrar_soporte/<int:pk>', views.cerrar_soporte, name="csn"),
     path('act_cliente/<int:pk>', views.act_cliente, name="ac"),
     path('getlogin/', views.login, name="gl"),
